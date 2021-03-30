ec6af04bee2039bf84a1f5ce117e70eb7e0f678c
Akash Butala <akash.butala.365@gmail.com>
2021-03-30 19:09:32 +0530

    ready to try deploying

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,22 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWthc2hidXRhbGEiLCJhIjoiY2ttb3BseGZ2MjZhYjJvcGZlcXBjcGFuNSJ9.qOOxPlNJlsscjQiX6A9J7A';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
